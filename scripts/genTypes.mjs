d0f94d4d9015ee2b95cf1a1a532a387309d62d13
Vitaly Turovsky <vital2580@icloud.com>
2024-01-21 11:12:43 +0530

    fix script, build

--- scripts/genTypes.mjs
+++ scripts/genTypes.mjs
@@ -1,6 +1,12 @@
+//@ts-check
 import fs from 'fs'
 
 const targetFile = './dist/types.d.ts';
-const modules = fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index')
-types = modules.filter(module => module.endsWith('.d.ts')).map(module => `import "./lib/modules/${module}"`).join('\n') + '\n' + types
+const plugins = fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index')
+let types = ''
+types = plugins.filter(module => module.endsWith('.d.ts')).map(module => `import "./lib/plugins/${module}"`).join('\n') + '\n' + types
 fs.writeFileSync(targetFile, types, 'utf8')
+
+let indexTs = fs.readFileSync('./dist/index.d.ts', 'utf8')
+indexTs = `import './types';` + indexTs
+fs.writeFileSync('./dist/index.d.ts', indexTs, 'utf8')
