34a75a284d50dd2f0e1be60da16be5febdd317f6
Vitaly <vital2580@icloud.com>
2024-01-21 23:25:17 +0530

    fix: publish dist folder

--- scripts/beforePublish.mjs
+++ scripts/beforePublish.mjs
@@ -3,4 +3,5 @@ import fs from 'fs'
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 delete packageJson.scripts.prepare
+packageJson.files = ['dist']
 fs.writeFileSync('package.json', JSON.stringify(packageJson, undefined, 2), 'utf8')
