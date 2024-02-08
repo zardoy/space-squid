c2164fdc50ddf792037e1f907a9689b1f4ee736a
Vitaly Turovsky <vital2580@icloud.com>
2024-02-08 23:50:16 +0300

    fix: found the culprit

--- scripts/beforePublish.mjs
+++ scripts/beforePublish.mjs
@@ -3,5 +3,4 @@ import fs from 'fs'
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 delete packageJson.scripts.prepare
-packageJson.files = ['dist']
 fs.writeFileSync('package.json', JSON.stringify(packageJson, undefined, 2), 'utf8')
