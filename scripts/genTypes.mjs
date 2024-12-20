730ddb70221fc61de40f67c0039eea8977585ec2
Vitaly Turovsky <vital2580@icloud.com>
2024-12-21 00:25:25 +0300

    chagne gentypes

--- scripts/genTypes.mjs
+++ scripts/genTypes.mjs
@@ -1,17 +1,24 @@
 //@ts-check
 import fs from 'fs'
 
-const targetFile = './dist/types.d.ts';
-const plugins = fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index')
-let types = ''
-types = plugins.filter(module => module.endsWith('.d.ts')).map(module => `import "./lib/modules/${module}"`).join('\n') + '\n' + types
-fs.writeFileSync(targetFile, types, 'utf8')
+const patchSource = process.argv.includes('--source');
 
-let indexTs = fs.readFileSync('./dist/index.d.ts', 'utf8')
-indexTs = `import './types';` + indexTs
-fs.writeFileSync('./dist/index.d.ts', indexTs, 'utf8')
+if (!patchSource) {
+    const targetFile = './dist/types.d.ts';
+    const plugins = fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index')
+    let types = ''
+    types = plugins.filter(module => module.endsWith('.d.ts')).map(module => `import "./lib/modules/${module}"`).join('\n') + '\n' + types
+    fs.writeFileSync(targetFile, types, 'utf8')
 
-const modules = fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index' && f.endsWith('.js')).map(f => f.replace('.js', ''))
+    let indexTs = fs.readFileSync('./dist/index.d.ts', 'utf8')
+    indexTs = `import './types';` + indexTs
+    fs.writeFileSync('./dist/index.d.ts', indexTs, 'utf8')
+}
+
+const modules =
+    patchSource
+    ? fs.readdirSync('./src/lib/modules').filter(f => f !== 'index.ts' && f.endsWith('.ts')).map(f => f.replace('.ts', ''))
+    : fs.readdirSync('./dist/lib/modules').filter(f => f !== 'index.js' && f.endsWith('.js')).map(f => f.replace('.js', ''))
 const modulesReqLines = modules.map(m => `'${m}': require('./${m}')`).join(',\n')
 const modulesFileJs = `
 module.exports = {
@@ -20,5 +27,5 @@ module.exports = {
     }
 }
 `
-const writePath = process.argv.includes('--source') ? './src/lib/modules/index.ts' : './dist/lib/modules/index.js'
+const writePath = patchSource ? './src/lib/modules/index.ts' : './dist/lib/modules/index.js'
 fs.writeFileSync(writePath, modulesFileJs, 'utf8')
