652db4b1e28410e756fe6a3c1526b021af2f77c8
Vitaly Turovsky <vital2580@icloud.com>
2025-03-20 00:46:51 +0300

    fix: dont use sync fs operation

--- src/lib/modules/pluginsFolder.ts
+++ src/lib/modules/pluginsFolder.ts
@@ -18,7 +18,7 @@ export const entity = function (entity: Entity, serv: Server) {
 export const server = async function (serv: Server, settings: Options) {
   loadedPlugins = {}
   if (!settings.pluginsFolder || !settings.worldFolder) return
-  const plugins = fs.readdirSync(path.join(settings.worldFolder, 'plugins'))
+  const plugins = await fs.promises.readdir(path.join(settings.worldFolder, 'plugins'))
 
   for (const plugin of plugins) {
     // match .js but not .disabled.js
